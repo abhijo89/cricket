74465bfaa75aeec757f7d6cce4af92c0ee3f60c1
Abhilash Joseph C <ouncebook@gmail.com>
2020-01-21 14:28:04 +0530

    Connector Issue

--- src/views.py
+++ src/views.py
@@ -1,15 +1,13 @@
-from utils.utils_api_base import RestBase
-from utils.utils_connector import MysqlConnector
+from utils.utils_api_base import RestBase, Processor
 from utils.utils_constants import Status
 
 
-class Top4Winners(RestBase):
+class Top4Winners(RestBase, Processor):
 
     URL = 'top-winners'
 
     def process_get(self):
-        conn = MysqlConnector()
-        cursor = conn.cursor()
-        cursor.execute("SELECT winner FROM matches WHERE season=2014 GROUP BY winner ORDER BY COUNT(*) DESC LIMIT 4;")
-        result_dict = cursor.fetchall()
+        sql = "SELECT winner FROM matches WHERE season=%s GROUP BY winner ORDER BY COUNT(*) DESC LIMIT 4"
+        args = (2014, ) # IT can going fwd dynamic , get from url param
+        result_dict = self.execute(sql, args)
         return Status.SUCCESS, None, None, result_dict
