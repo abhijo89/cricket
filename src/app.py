3dfb6356f7dac067319ca6782aef5ab5ea87b27b
Abhilash Joseph C <46181778+abhijo89-uc@users.noreply.github.com>
2020-01-21 15:34:28 +0530

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 
-from views import Top4Winners, Q2
+from views import Top4Winners, Q2,Q3, Q4,Q5,Q6
 from utils.utils_response import Response
 
 app = Flask(__name__)
@@ -8,6 +8,10 @@ app = Flask(__name__)
 # No blueprint for now :D , Will do it when we have muttiple module to define the url
 app.add_url_rule(f'/api/{Top4Winners.URL}/', view_func=Top4Winners.as_view('top_4_winners'))
 app.add_url_rule(f'/api/{Q2.URL}/', view_func=Q2.as_view('q2'))
+app.add_url_rule(f'/api/{Q3.URL}/', view_func=Q2.as_view('q3'))
+app.add_url_rule(f'/api/{Q4.URL}/', view_func=Q2.as_view('q4'))
+app.add_url_rule(f'/api/{Q5.URL}/', view_func=Q2.as_view('q5'))
+app.add_url_rule(f'/api/{Q6.URL}/', view_func=Q2.as_view('q6'))
 
 
 # In view i have many class, as above you can add all url for each view and expose this end URL's
