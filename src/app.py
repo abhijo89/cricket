eee779cc1d59547bd7ccc012b86039a758c69b12
Abhilash Joseph C <ouncebook@gmail.com>
2020-01-21 14:29:58 +0530

    Connector Issue

--- src/app.py
+++ src/app.py
@@ -1,10 +1,11 @@
 from flask import Flask
 
-from views import Top4Winners
+from views import Top4Winners, Q2
 app = Flask(__name__)
 
 # No blieprint for now :D , Will do it when we have muttiple module to define the url
 app.add_url_rule(f'/api/{Top4Winners.URL}/', view_func=Top4Winners.as_view('top_4_winners'))
+app.add_url_rule(f'/api/{Q2.URL}/', view_func=Q2.as_view('q2'))
 
 
 if __name__ == '__main__':
--- src/views.py
+++ src/views.py
@@ -11,3 +11,15 @@ class Top4Winners(RestBase, Processor):
         args = (2014, ) # IT can going fwd dynamic , get from url param
         result_dict = self.execute(sql, args)
         return Status.SUCCESS, None, None, result_dict
+
+class Q2(RestBase, Processor):
+
+    URL = 'q2'
+
+    def process_get(self):
+        sql = "SELECT toss_winner, COUNT(*) AS total FROM matches WHERE season=%s " \
+              "GROUP BY toss_winner ORDER BY total DESC LIMIT 1;"
+
+        args = (2014, ) # IT can going fwd dynamic , get from url param
+        result_dict = self.execute(sql, args)
+        return Status.SUCCESS, None, None, result_dict
